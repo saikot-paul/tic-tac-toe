3437bab18b2b1eb40e51a1a19604a831918d5261
saikot-paul <saikotpaul123@gmail.com>
2023-06-25 17:13:56 -0400

    minimax works

--- src/computer.js
+++ src/computer.js
@@ -34,25 +34,25 @@ class computer {
 
         while (!this.gameState()) { 
 
+            this.printBoard()
             
             if (this.currentTurn === this.human) { 
-                let choice = prompt(`Choose a position ${this.findOpen()}`)
+                let choice = prompt(`Player 1 choose a position ${this.findOpen()}`)
                 this.board[choice] = this.human 
                 this.currentTurn = this.comp 
             }else { 
-                let choice = prompt(`Choose a position ${this.findOpen()}`)
+                let choice = this.bestMove()
                 this.board[choice] = this.comp
                 this.currentTurn = this.human
             }
 
-            this.gameState()
+            this.printBoard()
         }
     }
 
     gameState() { 
 
         
-        this.printBoard()
         const { isWinHuman, isWinComp, isFull } = this.gameCondition()
 
         if (isWinHuman) { 
@@ -96,6 +96,52 @@ class computer {
         })
     }
 
+    checkWin(symbol) { 
+
+        let isWin = false 
+        this.wins.forEach( (element) => { 
+            let [index1, index2, index3] = element
+            const sym1 = this.board[index1]
+            const sym2 = this.board[index2]
+            const sym3 = this.board[index3]
+
+            if (sym1 && sym1 === sym2 && sym2 === sym3 && sym1 === symbol) { 
+                isWin = true
+            }
+        })
+
+        return isWin
+    }
+
+    checkFull () { 
+        return this.board.every( (element) => { 
+            return element !== null 
+        })
+    }
+
+    checkWin(symbol) { 
+
+        let isWin = false 
+        this.wins.forEach( (element) => { 
+            let [index1, index2, index3] = element
+            const sym1 = this.board[index1]
+            const sym2 = this.board[index2]
+            const sym3 = this.board[index3]
+
+            if (sym1 && sym1 === sym2 && sym2 === sym3 && sym1 === symbol) { 
+                isWin = true
+            }
+        })
+
+        return isWin
+    }
+
+    checkFull () { 
+        return this.board.every( (element) => { 
+            return element !== null 
+        })
+    }
+
     findOpen() { 
         let moves = [] 
 
@@ -107,15 +153,17 @@ class computer {
 
         return moves
     }
-
+    
     bestMove() {
-        return this.minimax(10, true).index
+        
+        let copy = this.board.map((element) => element)
+        return this.minimax(10, copy, true).index
     }
 
-    minimax (depth, max_player) { 
-
-        const { isWinHuman, isWinComp, isFull } = this.gameCondition()
-
+    minimax (depth, board, max_player) { 
+        
+        const { isWinHuman, isWinComp, isFull } = this._gameCondition(board)
+        
         if (isWinHuman) { 
             return -1
         }else if (isWinComp) { 
@@ -123,7 +171,7 @@ class computer {
         }else if (isFull) { 
             return 0
         }
-
+        
         if (depth === 0) { 
             return 0
         }
@@ -131,11 +179,11 @@ class computer {
         if (max_player) { 
             let best_move = null 
             let best_score = -Infinity
-
-            for (const index of this.findOpen()) { 
-                this.board[index] = this.comp
-                let score = this.minimax(depth-1, false)
-
+            
+            for (const index of this._findOpen(board)) { 
+                board[index] = this.comp
+                let score = this.minimax(depth-1, board, false)
+                
                 if (best_score < score) { 
                     best_score = score 
                     best_move = index
@@ -146,10 +194,10 @@ class computer {
             let best_move = null 
             let best_score = Infinity
 
-            for (const index of this.findOpen()) { 
-                this.board[index] = this.human
-                let score = this.minimax(depth-1, true)
-
+            for (const index of this._findOpen(board)) { 
+                board[index] = this.human
+                let score = this.minimax(depth-1, board, true)
+                
                 if (best_score > score) { 
                     best_score = score 
                     best_move = index
@@ -158,8 +206,51 @@ class computer {
             return {score: best_score, index: best_move}
         }  
     }
+    
+    _gameCondition(board) { 
+        const isWinHuman = this._checkWin(this.human, board)
+        const isWinComp = this._checkWin(this.comp, board)
+        const isFull = this._checkFull(board)
+        return { isWinHuman, isWinComp, isFull }
+    }
+    
+    _checkWin(symbol, board) { 
+        let isWin = false 
+        this.wins.forEach( (element) => { 
+            let [index1, index2, index3] = element
+            const sym1 = board[index1]
+            const sym2 = board[index2]
+            const sym3 = board[index3]
+
+            if (sym1 && sym1 === sym2 && sym2 === sym3 && sym1 === symbol) { 
+                isWin = true
+            }
+        })
+
+        return isWin
+    }
+    
+    _checkFull(board) { 
+        return board.every( (element) => { 
+            return element !== null 
+        })
+    }
+
+    _findOpen(board) { 
+        let moves = []
+
+        board.forEach( (element, index) => { 
+            if (element === null) { 
+                moves.push(index)
+            }
+        })
+        
+        return moves
+    }
+
 
 }
+
 
 const game = new computer() 
 game.playGame()
